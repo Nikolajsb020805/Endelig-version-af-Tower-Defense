635d498a1369b4990f77c4893579823ef619c152
Nikolajsb020805 <nschmidtbloch@gmail.com>
2026-04-09 20:39:44 +0200

    Update import statement for StartGame in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import fjende from './fjende.js';
 import Tower from './Tower.js';
-import StartGame from './startgame.js';
+import StartGame from './StartGame.js';
 import underspil from './underspil.js';
 import slutspil from './slutspil.js';
 
@@ -22,15 +22,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-// function create() {
-    
-    
-//     tower = new Tower(this, 600, 600);
-// }
-
-// function update() {
-//     if (tower) {
-//         tower.update(this.input.activePointer);
-//     }
-// }
